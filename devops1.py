36b32e3f3827ce656ccc157d1868af7376350e28
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2022-10-10 20:22:44 +0100

    edited some stuff

--- devops1.py
+++ devops1.py
@@ -1,4 +1,5 @@
 from cgitb import html
+from os import system
 import time
 import boto3
 import webbrowser
@@ -28,37 +29,8 @@ try:
         SecurityGroupIds=[
             'sg-0f7406fb4f316a5a0' # Security group ID
         ],
-        KeyName='MosesKeyPair' # Key pair name
-    )
-    
-
-except Exception as e:
-    print("An error occured while creating EC2 instance...")
-    print(e)
-    print("Exiting...")
-
-else:
-    print('\nInstance successfully created:' + # Print instance ID
-    '\n[ID: ' + new_instance[0].id + ']' + 
-    '\n[Type: ' + new_instance[0].instance_type + ']' + # Print instance type
-    '\n[Current state: ' + new_instance[0].state['Name'] + ']') # Print instance state
-    new_instance[0].wait_until_running() # Wait until instance is running
-    new_instance[0].reload() # Reload instance attributes
-    print('[Current state: ' + new_instance[0].state['Name'] + ']' +
-    '\n[Public IP: ' + new_instance[0].public_ip_address + ']\n') # Print instance public IP
-
-    # stop the instance after its running
-    print('Stopping instance to add meta data...')
-    new_instance[0].stop()
-    new_instance[0].wait_until_stopped()
-    new_instance[0].reload()
-    print('[Current state: ' + new_instance[0].state['Name'] + ']\n')
-
-    # edit the instance meta data
-    print('Adding meta data...')
-    new_instance[0].modify_attribute(
-        UserData={
-            'Value': '''#!/bin/bash
+        KeyName='MosesKeyPair', # Key pair name
+        UserData="""#!/bin/bash
             yum install httpd -y
             systemctl enable httpd
             systemctl start httpd
@@ -93,45 +65,58 @@ else:
 
             echo '</body>' >> index.html
             echo '</html>' >> index.html
-
             cp index.html /var/www/html/index.html
-            ''' 
-        }
+        """ 
     )
-    print('Meta data successfully added.\n')
-# 169.254.169.254/latest/meta-data/instance-id
-    # start the instance
-    print('Starting instance...')
-    new_instance[0].start()
-    new_instance[0].wait_until_running()
-    new_instance[0].reload()
-    print('[Current state: ' + new_instance[0].state['Name'] + ']\n')
+    
+
+except Exception as e:
+    print("An error occured while creating EC2 instance, check error.txt or console for more details")
+    print(e)
+    errorfile = open("error.log", "w")
+    errorfile.write(str(e))
+    errorfile.close()
+    print("Error file created")
+
+else:
+    print('\nInstance successfully created:' + # Print instance ID
+    '\n[ID: ' + new_instance[0].id + ']' + 
+    '\n[Type: ' + new_instance[0].instance_type + ']' + # Print instance type
+    '\n[Current state: ' + new_instance[0].state['Name'] + ']') # Print instance state
+    new_instance[0].wait_until_running() # Wait until instance is running
+    new_instance[0].reload() # Reload instance attributes
+    print('[Current state: ' + new_instance[0].state['Name'] + ']' +
+    '\n[Public IP: ' + new_instance[0].public_ip_address + ']\n') # Print instance public IP
 
     print("Waiting to be redirected to the webserver...") # Print message
-    time.sleep(15) # Wait 15 seconds
+    time.sleep(20) # Wait 15 seconds
     print("Opening web browser to : " + new_instance[0].public_ip_address) # Print message
 
     webbrowser.open('http://' + new_instance[0].public_ip_address) # Open web browser to instance public IP
+
+
+    # generate 6 random characters
+def randomString(stringLength=6):
+    letters = string.ascii_lowercase
+    numbers = string.digits
+    return ''.join(random.choice(letters + numbers) for i in range(stringLength))
 
 try:
     # create an S3 bucket
     print('\nCreating S3 bucket...')
     s3 = boto3.resource('s3')
+    s3_client = boto3.client('s3') # Create S3 client
 
-    # generate 6 random characters
-    def randomString(stringLength=6):
-        letters = string.ascii_lowercase
-        return ''.join(random.choice(letters) for i in range(stringLength))
-
-    new_bucket = s3.create_bucket(Bucket='mugwulo-' + randomString(),
+    new_bucket = s3.create_bucket(
+        Bucket='mugwulo-' + randomString(),
         ACL='public-read',
     ) 
     
     new_bucket.wait_until_exists() # wait until bucket exists
     print('Bucket successfully created: ' + new_bucket.name) # Print bucket name
-    print('\nEnabling static website hosting...') # enable static website hosting
 
-    new_bucket.Acl().put(ACL='public-read') # Make bucket public
+    print('\nEnabling static website hosting...') # enable static website hosting
+ 
 
     website_configuration = { 
         'ErrorDocument': {
@@ -143,32 +128,10 @@ try:
     }
 
     bucket_website = s3.BucketWebsite(new_bucket.name) # Create bucket website
+
     bucket_website.put(WebsiteConfiguration=website_configuration) # Put website configuration
+
     print('Static website hosting successfully enabled.') # Print message
-
-    s3_client = boto3.client('s3') # Create S3 client
-
-    print('Uploading files to S3 bucket...') # Print message
-    s3_client.upload_file('index.html', new_bucket.name, 'index.html',
-        ExtraArgs={
-            'ContentType': 'text/html',
-            'ACL': 'public-read',
-        } 
-    )
-
-    s3_client.upload_file('script.js', new_bucket.name, 'script.js',
-        ExtraArgs={
-            'ContentType': 'text/javascript',
-            'ACL': 'public-read',
-        }
-    )
-
-    s3_client.upload_file('style.css', new_bucket.name, 'style.css',
-        ExtraArgs={
-            'ContentType': 'text/css',
-            'ACL': 'public-read',
-        }
-    )
 
 
     # download a file from a url
@@ -184,26 +147,38 @@ try:
         }
     )
 
-    # set permissions for the files
-    print('\nSetting permissions for files...') # Print message
-    new_bucket.Object('index.html').Acl().put(ACL='public-read') # Set index.html permissions
-    new_bucket.Object('style.css').Acl().put(ACL='public-read') # Set style.css permissions
-    new_bucket.Object('script.js').Acl().put(ACL='public-read') # Set script.js permissions
-    s3_client.put_object_acl(ACL='public-read', Bucket=new_bucket.name, Key='logo.jpg') # Set logo.jpg permissions
-    print('Permissions successfully set.') # Print message
-
-
-    # put the image into the index.html file
-    print('\nAdding image to index.html...') # Print message
-    index_html = new_bucket.Object('index.html').get()['Body'].read().decode('utf-8') # Get index.html file
-    index_html1 = index_html.replace('logo.jpg', 'https://' + new_bucket.name + '.s3.amazonaws.com/logo.jpg') # Replace logo.jpg with url
-    new_bucket.Object('index.html').put(Body=index_html1) # Put index.html file
-    print('Image successfully added.') # Print message
+    print('Uploading files to S3 bucket...') # Print message
+    s3_client.upload_file('index.html', new_bucket.name, 'index.html',
+        ExtraArgs={
+            'ContentType': 'text/html',
+            'ACL': 'public-read',
+        } 
+    )
 
     print('\nOpening web browser to : ' + new_bucket.name + '.s3-website-us-east-1.amazonaws.com') # Print message
     webbrowser.open('http://' + new_bucket.name + '.s3-website-us-east-1.amazonaws.com') # Open web browser to bucket website
 
 except Exception as e:
-    print("An error occured while creating S3 bucket...")
+    print("An error occured while creating S3 bucket, check error.txt or console for more details")
     print(e)
-    print("Exiting...")
+    errorfile = open("error.log", "w")
+    errorfile.write(str(e))
+    errorfile.close()
+    print("Error file created")
+
+try:
+    # write both URLs to a file
+    print('\nWriting URLs to file...')
+    file = open('mugwulo.txt', 'w')
+    file.write('Instance URL: http://' + new_instance[0].public_ip_address + '\nS3 Bucket URL: http://' + new_bucket.name + '.s3-website-us-east-1.amazonaws.com')
+    file.close()
+    print('File successfully written.\n')
+
+except Exception as e:
+    print("An error occured while writing URLs to file, check error.txt or console for more details")
+    print(e)
+    errorfile = open("error.log", "w")
+    errorfile.write(str(e))
+    errorfile.close()
+    print("Error file created")
+
